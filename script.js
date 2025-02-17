8b371127a5ee8c2ec12cce968a9c8318ff121508
sharadverma <sharadverma175@gmail.com>
2025-02-17 13:44:47 +0530

    .

---
+++ script.js
@@ -0,0 +1,4 @@
+// Here we can add custom JavaScript functionality if needed
+// Currently, there's no dynamic behavior, but if you plan to add interactivity, use this script.
+
+console.log("Welcome to Sharad Verma's Portfolio");
